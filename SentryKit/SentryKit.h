caffc556481ce1178d0dff2f2437f78415f8072d
David Chavez <david@dcvz.io>
2016-09-03 01:09:06 +0200

    Initial Commit

---
+++ SentryKit/SentryKit.h
@@ -0,0 +1,19 @@
+//
+//  SentryKit.h
+//  SentryKit
+//
+//  Created by David Chavez on 9/3/16.
+//  Copyright © 2016 David Chavez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SentryKit.
+FOUNDATION_EXPORT double SentryKitVersionNumber;
+
+//! Project version string for SentryKit.
+FOUNDATION_EXPORT const unsigned char SentryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SentryKit/PublicHeader.h>
+
+
